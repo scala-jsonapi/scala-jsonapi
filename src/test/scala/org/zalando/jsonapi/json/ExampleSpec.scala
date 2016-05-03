89abab5b4e9f1106b38cfc40bd03a906c5e2a2cf
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-05-03 14:49:34 +0200

    Allow links of any names

--- src/test/scala/org/zalando/jsonapi/json/ExampleSpec.scala
+++ src/test/scala/org/zalando/jsonapi/json/ExampleSpec.scala
@@ -1,11 +1,11 @@
 package org.zalando.jsonapi.json
 
-import org.scalatest.{ MustMatchers, WordSpec }
-import org.zalando.jsonapi.json.sprayjson.{ SprayJsonJsonapiProtocol, SprayJsonJsonapiProtocol$ }
-import org.zalando.jsonapi.{ JsonapiRootObjectWriter, _ }
+import org.scalatest.{MustMatchers, WordSpec}
+import org.zalando.jsonapi.json.sprayjson.{SprayJsonJsonapiProtocol, SprayJsonJsonapiProtocol$}
+import org.zalando.jsonapi.{JsonapiRootObjectWriter, _}
 import org.zalando.jsonapi.model.JsonApiObject.StringValue
 import org.zalando.jsonapi.model.RootObject.ResourceObject
-import org.zalando.jsonapi.model.{ Attribute, Links, RootObject }
+import org.zalando.jsonapi.model.{Attribute, Link, Links, RootObject}
 import spray.json._
 
 class ExampleSpec extends WordSpec with MustMatchers with SprayJsonJsonapiProtocol {
@@ -52,7 +52,7 @@ class ExampleSpec extends WordSpec with MustMatchers with SprayJsonJsonapiProtoc
             id = Some(person.id.toString),
             attributes = Some(List(
               Attribute("name", StringValue(person.name))
-            )), links = Some(List(Links.Self("http://test.link/person/42"))))))
+            )), links = Some(List(Link("self", "http://test.link/person/42"))))))
         }
       }
 
@@ -85,7 +85,7 @@ class ExampleSpec extends WordSpec with MustMatchers with SprayJsonJsonapiProtoc
             id = Some(person.id.toString),
             attributes = Some(List(
               Attribute("name", StringValue(person.name))
-            )))), links = Some(List(Links.Next("http://test.link/person/43"))))
+            )))), links = Some(List(Link("next", "http://test.link/person/43"))))
         }
       }
 
