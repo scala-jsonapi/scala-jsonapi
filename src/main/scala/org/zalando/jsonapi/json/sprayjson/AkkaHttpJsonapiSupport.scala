2c01da9be193e5923d6c75f464004c750efb9e66
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-05-26 16:50:24 +0200

    improve (un)marshalling after review

--- src/main/scala/org/zalando/jsonapi/json/sprayjson/AkkaHttpJsonapiSupport.scala
+++ src/main/scala/org/zalando/jsonapi/json/sprayjson/AkkaHttpJsonapiSupport.scala
@@ -1,22 +1,19 @@
-package org.zalando.jsonapi.json.sprayjson
+package org.zalando.jsonapi.json
+package sprayjson
 
-import akka.http.scaladsl.marshalling.{ ToEntityMarshaller, Marshaller }
+import akka.http.scaladsl.marshalling.{ Marshaller, ToEntityMarshaller }
 import org.zalando.jsonapi.model.RootObject
 import spray.json._
 import akka.http.scaladsl.model.MediaTypes.`application/vnd.api+json`
 import akka.http.scaladsl.unmarshalling.{ FromEntityUnmarshaller, Unmarshaller }
+import org.zalando.jsonapi._
 
-trait AkkaHttpJsonapiSupport extends SprayJsonJsonapiFormat with DefaultJsonProtocol {
-  implicit def akkaHttpJsonJsonapiMarshaller(implicit printer: JsonPrinter = PrettyPrinter): ToEntityMarshaller[RootObject] = {
-    Marshaller.StringMarshaller.wrap(`application/vnd.api+json`)(printer).compose(_.toJson)
-  }
+trait AkkaHttpJsonapiSupport extends DefaultJsonProtocol with SprayJsonJsonapiFormat {
+  implicit def akkaHttpJsonJsonapiMarshaller[T: JsonapiRootObjectWriter]: ToEntityMarshaller[T] =
+    Marshaller.withFixedContentType(`application/vnd.api+json`)(_.rootObject.toJson.compactPrint)
 
-  implicit def akkaHttpJsonJsonapiUnmarshaller: FromEntityUnmarshaller[RootObject] = {
-    Unmarshaller
-      .byteStringUnmarshaller
-      .forContentTypes(`application/vnd.api+json`)
-      .mapWithCharset((data, charset) ⇒ data.decodeString(charset.nioCharset.name).parseJson.convertTo[RootObject])
-  }
+  implicit def akkaHttpJsonJsonapiUnmarshaller[T: JsonapiRootObjectReader]: FromEntityUnmarshaller[T] =
+    Unmarshaller.stringUnmarshaller.forContentTypes(`application/vnd.api+json`).map(_.parseJson.convertTo[RootObject].jsonapi[T])
 }
 
 object AkkaHttpJsonapiSupport extends AkkaHttpJsonapiSupport
