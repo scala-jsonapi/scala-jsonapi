f22c681355b56dd226739ab4fc07bfe4e24aa8c3
j <emailme.jtg+github.com>
2016-09-16 16:36:35 -0700

    implicit conversions for all jsonapiobject values

--- src/main/scala/org/zalando/jsonapi/model/implicits/JsonApiObjectValueConversions.scala
+++ src/main/scala/org/zalando/jsonapi/model/implicits/JsonApiObjectValueConversions.scala
@@ -2,13 +2,43 @@ package org.zalando.jsonapi.model.implicits
 
 import scala.language.implicitConversions
 
+import org.zalando.jsonapi.model.Attribute
 import org.zalando.jsonapi.model.JsonApiObject._
 
 object JsonApiObjectValueConversions {
-  implicit def convertStringToStringValue(string: String) = StringValue(string)
-  implicit def convertIntToNumberValue(int: Int) = NumberValue(int)
-  implicit def convertLongToNumberValue(long: Long) = NumberValue(long)
-  implicit def convertDoubleToNumberValue(double: Double) = NumberValue(double)
-  implicit def convertFloatToNumberValue(float: Float) = NumberValue(float)
-  implicit def convertBooleanToBooleanValue(boolean: Boolean) = BooleanValue(boolean)
+  implicit def convertAnyToValue(any: Any): Value = {
+    any match {
+      case(string: String) ⇒
+        StringValue(string)
+      case(int: Int) ⇒
+        NumberValue(int)
+      case(long: Long) ⇒
+        NumberValue(long)
+      case(double: Double) ⇒
+        NumberValue(double)
+      case(float: Float) ⇒
+        NumberValue(float)
+      case true ⇒
+        TrueValue
+      case false ⇒
+        FalseValue
+      case null ⇒
+        NullValue
+      case value: Value ⇒
+        value
+      case(map: Map[_,_]) ⇒
+        JsObjectValue(
+          map.map {
+            case(name: String, value) ⇒
+              Attribute(name, convertAnyToValue(value))
+            case _ ⇒
+              throw UnconvertibleTypeError("Maps must have string keys to be converted to JsonApiObject Values")
+          }.toList
+        )
+      case(seq: Seq[_]) ⇒
+        JsArrayValue(seq.map(convertAnyToValue))
+      case _ ⇒
+        throw UnconvertibleTypeError(any)
+   }
+  }
 }
