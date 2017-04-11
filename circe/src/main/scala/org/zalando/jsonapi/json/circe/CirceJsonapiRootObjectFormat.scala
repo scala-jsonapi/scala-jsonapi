7d232a3a7f3ef7cffb4194df1ece102694bb985c
j <emailme.jtg+github@gmail.com>
2017-04-11 14:47:45 -0700

    new CirceJsonapiRootObjectReader (implicit encoder => regular defs)

--- circe/src/main/scala/org/zalando/jsonapi/json/circe/CirceJsonapiRootObjectFormat.scala
+++ circe/src/main/scala/org/zalando/jsonapi/json/circe/CirceJsonapiRootObjectFormat.scala
@@ -3,7 +3,6 @@ package org.zalando.jsonapi.json.circe
 import io.circe.Json
 import io.circe.generic.auto._
 import io.circe.syntax._
-import org.zalando.jsonapi.json.circe.CirceJsonapiEncoders._
 import org.zalando.jsonapi.json.circe.CirceJsonapiDecoders._
 import org.zalando.jsonapi.{JsonapiRootObjectWriter, JsonapiRootObjectReader}
 import org.zalando.jsonapi.model.RootObject
@@ -13,12 +12,6 @@ trait CirceJsonapiRootObjectWriter extends JsonapiRootObjectWriter[Json] {
 }
 
 case object CirceJsonapiRootObjectWriter extends CirceJsonapiRootObjectWriter
-
-trait CirceJsonapiRootObjectReader extends JsonapiRootObjectReader[Json] {
-  def fromJsonapi(rootObject: RootObject): Json = rootObject.asJson
-}
-
-case object CirceJsonapiRootObjectReader extends CirceJsonapiRootObjectReader
 
 trait CirceJsonapiRootObjectFormat extends CirceJsonapiRootObjectWriter with CirceJsonapiRootObjectReader
 
